d7a3890bcc4efb80845e0c88989932c214fb61d9
pierrevaut <p.vautherin@hotmail.com>
2020-05-21 12:41:46 +0200

    [ADD] Hello world

---
+++ lib/app.ts
@@ -0,0 +1,13 @@
+import * as bodyParser from "body-parser";
+
+import express = require('express');
+
+const app: express.Application = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello Worldzz!');
+});
+
+app.listen(3000, function () {
+  console.log('App is listening on port 3000!');
+});
